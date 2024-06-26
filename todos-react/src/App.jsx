0f05e1262ef2692e85c102dc54c9ff17464808a3
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-06-26 14:10:11 +0200

    Correction

--- todos-react/src/App.jsx
+++ todos-react/src/App.jsx
@@ -1,24 +1,57 @@
 // import { useState } from 'react'
+import { useState } from 'react';
 import './App.css';
 import TodoItem from './TodoItem';
 
 function App() {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 123, title: 'ABC', completed: false },
     { id: 456, title: 'DEF', completed: true },
     { id: 789, title: 'XYZ', completed: false },
-  ];
+  ]);
+  const [newTodo, setNewTodo] = useState('ABC');
   const editingId = 789;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    setTodos([
+      ...todos,
+      { id: Math.random(), title: newTodo, completed: false },
+    ]);
+    setNewTodo('');
+  }
+
+  function handleToggleClick(event) {
+    const globalChecked = event.target.checked;
+
+    setTodos(
+      // idéalement ne pas tout cloner (plus simple avec useImmer) :
+      // todos.map((todo) => todo.completed === globalChecked ? todo : ({ ...todo, completed: globalChecked }))
+      todos.map((todo) => ({ ...todo, completed: globalChecked }))
+    );
+  }
 
   return (
     <>
-      <form className="todos-form">
-        <input type="checkbox" className="todos-toggle-checked" />
-        <input type="text" className="todos-new-input" />
+      <form className="todos-form" onSubmit={handleSubmit}>
+        <input type="checkbox" className="todos-toggle-checked" onChange={handleToggleClick} />
+        <input
+          type="text"
+          className="todos-new-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
         <button>+</button>
       </form>
       <div className="todos-container">
-        {todos.map((todo) => <TodoItem key={todo.id} todo={todo} isEditing={todo.id === editingId} />)}
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            isEditing={todo.id === editingId}
+          />
+        ))}
       </div>
     </>
   );
